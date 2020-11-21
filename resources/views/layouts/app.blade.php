881daf40a496bd46ae213b4cf99aa291fd7c6d13
zaxwebs <zifidigital@gmail.com>
2020-11-22 02:32:21 +0530

    fixed dropdown positioning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
 				</li>
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="dropdown-p" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-					<div class="dropdown-menu" aria-labelledby="dropdown-p">
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-p">
 						<a class="dropdown-item" href="#">Action</a>
 						<a class="dropdown-item" href="#">Another action</a>
 						<a class="dropdown-item" href="#">Something else here</a>
