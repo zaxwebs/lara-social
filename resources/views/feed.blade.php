383da765a091b84e3e83f6d2f2353b15e1732043
zaxwebs <zifidigital@gmail.com>
2020-11-23 09:03:04 +0530

    added post publish form

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="row">
+	<div class="col-md-6 col-lg-4">
+		<div class="panel">
+			<form method="post" action="#">
+				<div class="form-group">
+					<label for="post">What's on your mind?</label>
+					<textarea class="form-control" id="post" rows="5"></textarea>
+				</div>
+				<button type="submit" class="btn btn-primary">Publish your post</button>
+			</form>
+		</div>
+	</div>
+</div>
 @endsection
