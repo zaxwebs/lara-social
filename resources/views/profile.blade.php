2743c23db80df4b373a084dbac3d9ec002885586
zaxwebs <zifidigital@gmail.com>
2020-11-24 18:43:55 +0530

    added post count and dummy about

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,6 +5,14 @@
 	<div class="col-lg-6 col-xl-4 offset-xl-4">
 		<div class="panel">
 			<h1>{{ $user->full_name }}</h1>
+			<div class="mb-3">
+				<h5>About</h5>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus arcu sem, consequat sit amet tincidunt id, egestas imperdiet ipsum. Aenean nec tristique ante.
+			</div>
+			<div>
+				<h5>Posts</h5>
+				{{ $user->posts->count() }}
+			</div>
 		</div>
 
 		@if($user->username === auth()->user()->username)
