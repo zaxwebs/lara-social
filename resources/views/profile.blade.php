a41a9da2cbe7d63b3f99862a703c2c65d4ff175c
zaxwebs <zifidigital@gmail.com>
2020-11-27 01:36:57 +0530

    added links to view highlighted posts

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -70,6 +70,14 @@
 		</div>
 		@endif
 
+		<div class="d-flex justify-content-between align-items-center mb-3">
+			<h4 class="mb-0">{{ request()->has('highlights') ? 'Highlighted Posts' : 'Posts'}}</h4>
+			<div class="btn-group" role="group">
+				<a class="mr-3" href="{{ route('profile', $user) }}">All Posts</a>
+				<a href="{{ route('profile', $user, ['highlights', 1]) }}?highlights=1">Highlights</a>
+			</div>
+		</div>
+
 		@foreach ($posts as $post)
 				<div class="panel">
 					<div class="d-flex justify-content-between">
