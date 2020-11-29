96861ea56d04b4831888e8d329a6a094bab6a31f
zaxwebs <zifidigital@gmail.com>
2020-11-29 11:25:23 +0530

    created post include

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -79,30 +79,7 @@
 		</div>
 
 		@foreach ($posts as $post)
-				<div class="panel">
-					<div class="d-flex justify-content-between">
-						<div><a href="{{ route('profile') }}/{{ $post->user->username }}">{{ $post->user->full_name }}</a></div>
-						<small class="text-muted">{{ $post->created_at->diffForHumans() }}</small>
-					</div>
-					<div class="mb-2">
-					{!! nl2br(e($post->body)) !!}
-					</div>
-					<div>
-						@if($user->is_viewer)
-							@if(!$post->is_highlighted)
-								<form method="post" action="{{ route('highlight', $post) }}">
-									@csrf
-									<button class="btn btn-link btn-sm p-0">Highlight</button>
-								</form>
-							@else
-								<form method="post" action="{{ route('unhighlight', $post) }}">
-									@csrf
-									<button class="btn btn-link btn-sm p-0">Highlighted</button>
-								</form>
-							@endif 
-						@endif
-					</div>
-				</div>
+			@include('includes.post')
 		@endforeach
 		{{ $posts->appends(request()->query())->links() }}
 
