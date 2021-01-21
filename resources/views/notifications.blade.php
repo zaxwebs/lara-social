2ce7f54bb54f79f6b7b058b449ced0e3d94a6809
zaxwebs <zifidigital@gmail.com>
2021-01-21 08:31:52 +0530

    Updated notifications rendering

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -5,12 +5,12 @@
 <h4 class="mb-3">Notifications</h4>
 
 @foreach ($notifications as $notification)
-<div class="panel">
+<div class="mb-4">
 	@if($notification->type === 'App\Notifications\UserFollowed')
 	<a href="{{ route('profile', $notification->models['User']) }}">{{ $notification->models['User']->full_name }}</a>
 	followed you.
 	@elseif($notification->type === 'App\Notifications\PostLiked')
-	<div class="mb-4">
+	<div class="mb-2">
 		<a href="{{ route('profile', $notification->models['User']) }}">{{ $notification->models['User']->full_name }}</a>
 		liked your post.
 	</div>
